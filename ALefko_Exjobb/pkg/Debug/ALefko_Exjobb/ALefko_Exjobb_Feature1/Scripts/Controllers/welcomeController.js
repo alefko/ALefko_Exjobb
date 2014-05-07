0b653ad5d74c8689e8f0506d058588f3eeca02d3
Andreas Lefkopoulos <andreas@alefko.com>
2014-05-07 10:41:18 +0200

    Added all file again

---
+++ ALefko_Exjobb/pkg/Debug/ALefko_Exjobb/ALefko_Exjobb_Feature1/Scripts/Controllers/welcomeController.js
@@ -0,0 +1,13 @@
+﻿mainApp.controller('startController', function ($scope, welcomeFactory, sharePointFactory) {
+
+    $(document).ready(
+        function() {
+            init();
+        }
+    );
+
+    function init() {
+        $scope.createSubsite = sharePointFactory.createSubsite();
+    }
+    
+});
